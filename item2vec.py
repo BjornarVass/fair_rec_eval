bc4a2451a9c636809d497ce9c07fe0dbae54ec16
Bjornar Vassoy <bjornar.vass@gmail.com>
2025-05-22 14:39:57 +0200

    Remove old functions, rename new

--- item2vec.py
+++ item2vec.py
@@ -25,9 +25,9 @@ class SkipGram(nn.Module):
 
 
 class SkipGramDataset(Dataset):
-    def __init__(self, data):
-        self.x = torch.tensor(data[:, 0], dtype=torch.int64)
-        self.y = torch.tensor(data[:, 1], dtype=torch.int64)
+    def __init__(self, data, device="cuda"):
+        self.x = torch.tensor(data[:, 0], dtype=torch.int64, device=device)
+        self.y = torch.tensor(data[:, 1], dtype=torch.int64, device=device)
         self.n = self.x.shape[0]
 
     def __len__(self):
@@ -37,7 +37,7 @@ class SkipGramDataset(Dataset):
         return self.x[idx], self.y[idx]
 
 
-def fit_item2vec2(x, min_count, tr_batch, embedding_dim, dim2, device, dataset_path, verbose, store=False):
+def fit_item2vec(x, min_count, tr_batch, embedding_dim, dim2, device, dataset_path, verbose, store=False):
     n_es = 5
     n_u = x.shape[0]
     n_i = x.shape[1]
@@ -58,7 +58,9 @@ def fit_item2vec2(x, min_count, tr_batch, embedding_dim, dim2, device, dataset_p
             va_x[user, j] = 1
             tr_x[user, j] = 0
 
-    unique_recs, counts = np.unique(tr_x.nonzero(), return_counts=True)
+    # np.nonzero() is used to count unique item occurences. The first output is ignored as it is the user index of each item occurence
+    _, item_inds = tr_x.nonzero()
+    unique_recs, counts = np.unique(item_inds, return_counts=True)
     if verbose:
         print_rec_dist(n_u, n_i, counts)
 
@@ -69,24 +71,22 @@ def fit_item2vec2(x, min_count, tr_batch, embedding_dim, dim2, device, dataset_p
             reindex_map[old_ind] = n_rec_i
             n_rec_i += 1
 
-    va_data = item2vec_data2(va_x, reindex_map)
-    va_loader = DataLoader(
-        SkipGramDataset(va_data), batch_size=va_data.shape[0] // 20, generator=torch.Generator(device=device)
-    )
+    va_data = item2vec_data(va_x, reindex_map)
+    va_loader = DataLoader(SkipGramDataset(va_data), batch_size=va_data.shape[0] // 20)
 
     model = SkipGram(n_rec_i, embedding_dim, dim2)
+    model.to(device)
     optimizer = optim.Adam(model.parameters())
     loss_function = nn.NLLLoss()
     epoch = 0
     best_loss = np.inf
-    while epoch < 100:
-        tr_data = item2vec_data2(tr_x, reindex_map)
-        tr_loader = DataLoader(
-            SkipGramDataset(tr_data), batch_size=tr_batch, shuffle=True, generator=torch.Generator(device=device)
-        )
+    eps = 1e-4
+    while epoch < 1000:
+        tr_data = item2vec_data(tr_x, reindex_map)
+        tr_loader = DataLoader(SkipGramDataset(tr_data), batch_size=tr_batch, shuffle=True)
         model.train()
         losses = []
-        for x, y in tr_loader:
+        for i, (x, y) in enumerate(tr_loader):
             log_probs = model(x)
             optimizer.zero_grad()
             loss = loss_function(log_probs, y)
@@ -105,9 +105,14 @@ def fit_item2vec2(x, min_count, tr_batch, embedding_dim, dim2, device, dataset_p
             print(f"{epoch} Train: {np.mean(losses):.4f}, Val: {np.mean(va_losses):.4f}")
         # Early stopping
         if mean_loss < best_loss:
-            best_loss = mean_loss
-            improv_counter = 0
             best_model = copy.deepcopy(model.state_dict())
+            if best_loss - mean_loss > eps:
+                improv_counter = 0
+            else:
+                improv_counter += 1
+                if improv_counter == n_es:
+                    break
+            best_loss = mean_loss
         else:
             improv_counter += 1
             if improv_counter == n_es:
@@ -122,7 +127,7 @@ def fit_item2vec2(x, min_count, tr_batch, embedding_dim, dim2, device, dataset_p
     return model.embeddings, reindex_map
 
 
-def item2vec_data2(x, reindex_map):
+def item2vec_data(x, reindex_map):
     training_data = []
     for user_ind in range(x.shape[0]):
         user_items = x[user_ind].nonzero()[0]
@@ -148,86 +153,6 @@ def item2vec_data2(x, reindex_map):
                     continue
                 training_data.append([input_item, target_item])
                 n_added += 1
-    return np.array(training_data, dtype=np.int64)
-
-
-def fit_item2vec(
-    tr_x, tr_p, va_p, min_count, window_size, top_k, tr_batch, embedding_dim, dim2, n_epochs, device, store=False
-):
-    n_u = tr_x.shape[0]
-    n_i = tr_x.shape[1]
-
-    top_recs = np.argsort(-tr_p, axis=1)[:, :top_k]
-    unique_recs, counts = np.unique(top_recs, return_counts=True)
-    print_rec_dist(n_u, n_i, counts)
-
-    reindex_map = {}
-    n_rec_i = 0
-    for i, old_ind in enumerate(unique_recs):
-        if counts[i] >= min_count:
-            reindex_map[old_ind] = n_rec_i
-            n_rec_i += 1
-
-    tr_data = item2vec_data(tr_p, top_k, reindex_map, window_size)
-    va_data = item2vec_data(va_p, top_k, reindex_map, window_size)
-    tr_loader = DataLoader(
-        SkipGramDataset(tr_data), batch_size=tr_batch, shuffle=True, generator=torch.Generator(device=device)
-    )
-    va_loader = DataLoader(
-        SkipGramDataset(va_data), batch_size=va_data.shape[0] // 20, generator=torch.Generator(device=device)
-    )
-
-    model = SkipGram(n_rec_i, embedding_dim, dim2)
-    optimizer = optim.Adam(model.parameters())
-    loss_function = nn.NLLLoss()
-    for epoch in range(n_epochs):
-        model.train()
-        losses = []
-        for x, y in tr_loader:
-            log_probs = model(x)
-            optimizer.zero_grad()
-            loss = loss_function(log_probs, y)
-            loss.backward()
-            optimizer.step()
-            losses.append(loss.item())
-        model.eval()
-        with torch.no_grad():
-            va_losses = []
-            for x, y in va_loader:
-                log_probs = model(x)
-                loss = loss_function(log_probs, y)
-                va_losses.append(loss.item())
-        print(f"{epoch}/{n_epochs} Train: {np.mean(losses):.4f}, Val: {np.mean(va_losses):.4f}")
-
-    if store:
-        torch.save(model.embeddings.state_dict(), "embs.pt")
-        with open("reindex_map.pkl", "wb") as f:
-            pickle.dump(reindex_map, f)
-
-    return model.embeddings, reindex_map
-
-
-def item2vec_data(probs, top_k, reindex_map, window_size):
-    top_recs = np.argsort(-probs, 1)[:, :top_k]
-    training_data = []
-    incs = list(range(-window_size, window_size))
-    incs = [x + 1 if x >= 0 else x for x in incs]
-    for user_ind in range(top_recs.shape[0]):
-        recs = top_recs[user_ind]
-        valid_recs = []
-        for rec in recs:
-            if rec in reindex_map:
-                valid_recs.append(reindex_map[rec])
-
-        n_valid = len(valid_recs)
-        if n_valid <= 1:
-            continue
-
-        for i in range(n_valid):
-            for inc in incs:
-                inc_ind = i + inc
-                if inc_ind >= 0 and inc_ind < n_valid:
-                    training_data.append([valid_recs[inc_ind], valid_recs[i]])
     return np.array(training_data, dtype=np.int64)
 
 
